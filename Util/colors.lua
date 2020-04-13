e533123bc80fd3ef792cc371a6c7e50587c486ed
Stan <spammyboy@protonmail.com>
2020-04-13 23:49:53 +0100

    refractor, see description
    - clearer function names
    - using class module
    - readability ++++++ (variable name changes + comments)
    - now using ternary statment for alpha in both functions instead of if

--- Util/colors.lua
+++ Util/colors.lua
@@ -1,39 +1,34 @@
--- modified by Stan O (dmoa) from https://github.com/andrewyavors/Lua-Color-Converter
+local Object = require "classic" -- temp
 
-local M = {
-    default = {
+local Colors = Object:extend()
+
+function Colors:new()
+	self.default = {
         "#2E3440", "#3B4252", "#434C5E", "#4C566A",
         "#D8DEE9", "#E5E9F0", "#ECEFF4", "#8FBCBB",
         "#88C0D0", "#81A1C1", "#5E81AC", "#BF616A",
         "#D08770", "#EBCB8B", "#A3BE8C", "#B48EAD",
     }
-}
-
-local function to_rgb (hex, alpha)
-    hex = hex:gsub('%#', '')
-	local redColor,greenColor,blueColor=hex:match('(..)(..)(..)')
-	redColor, greenColor, blueColor = tonumber(redColor, 16)/255, tonumber(greenColor, 16)/255, tonumber(blueColor, 16)/255
-	redColor, greenColor, blueColor = math.floor(redColor*100)/100, math.floor(greenColor*100)/100, math.floor(blueColor*100)/100
-	if alpha == nil then
-		return redColor, greenColor, blueColor
-	elseif alpha > 1 then
-		alpha = alpha / 100
-	end
-	return redColor, greenColor, blueColor, alpha
 end
 
-local function to_hex (r, g, b, alpha)
-	local redColor,greenColor,blueColor=r/255, g/255, b/255
-	redColor, greenColor, blueColor = math.floor(redColor*100)/100, math.floor(greenColor*100)/100, math.floor(blueColor*100)/100
-	if alpha == nil then
-		return redColor, greenColor, blueColor
-	elseif alpha > 1 then
-		alpha = alpha / 100
-	end
-	return redColor, greenColor, blueColor, alpha
+-- to_rgb functions return all values 0->1
+
+function Colors:to_rgb (hex, _a)
+	local r, g, b = hex:gsub('%#', ''):match('(..)(..)(..)') -- removes possible "#" and splits values
+	r = math.floor(tonumber(r, 16) / 255 * 100) / 100
+	g = math.floor(tonumber(g, 16) / 255 * 100) / 100
+	b = math.floor(tonumber(b, 16) / 255 * 100) / 100
+	a = _a and _a / 100 or 1
+	return r, g, b, a
 end
 
-M.to_rgb = to_rgb
-M.to_hex = to_hex
+function Colors:rrgb_to_rgb (_r, _g, _b, _a)
+	r = math.floor(_r / 255 * 100) / 100
+	g = math.floor(_g / 255 * 100) / 100
+	b = math.floor(_b / 255 * 100) / 100
+	a = _a and _a / 100 or 1
 
-return M
+	return r, g, b, a
+end
+
+return Colors
