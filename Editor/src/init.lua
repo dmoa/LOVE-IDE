6aba77500667e6e8bb8b76990847cd214887342d
Stan <spammyboy@protonmail.com>
2020-04-14 19:02:33 +0100

    slight reshuffle

--- Editor/src/init.lua
+++ Editor/src/init.lua
@@ -1,31 +1,15 @@
+local TextEditor = Object:extend()
+
 local Font = love.graphics.newFont("Editor/src/FiraCode-Regular.ttf")
 
-local TextEditor = {}
-TextEditor.mt = {__index = TextEditor}
-
-function TextEditor.new(w, h)
-	local textEditor = setmetatable({
-		cursor = {
-			pos = 1,
-			line = 1,
-		},
-
-		canvas = nil,
-		file = {""}
-	}, TextEditor.mt)
-
-	textEditor:__rebuildCanvas(w, h)
+function TextEditor:new(w, h)
+	self.cursor = {
+		pos = 1,
+		line = 1
+	}
+	self.file = {""}
 
 	return textEditor
-end
-
-function TextEditor:__rebuildCanvas(w, h)
-	if (self.canvas) then
-		self.canvas:release()
-		self.canvas = nil
-	end
-
-	self.canvas = love.graphics.newCanvas(w, h)
 end
 
 function TextEditor:update(dt)
@@ -33,15 +17,12 @@ end
 
 function TextEditor:draw(...)
 	love.graphics.push("all")
-		love.graphics.setCanvas(self.canvas)
 		love.graphics.clear(0, 0, 0, 0)
 		love.graphics.setFont(Font)
 
 		love.graphics.setColor(1, 1, 1, 1)
 		love.graphics.print(table.concat(self.file, "\n"))
 	love.graphics.pop()
-
-	love.graphics.draw(self.canvas, ...)
 end
 
 function TextEditor:textInput(t)
@@ -68,10 +49,4 @@ function TextEditor:keyPressed(key)
 	end
 end
 
-function TextEditor:resize(w, h)
-	self:__rebuildCanvas(w, h)
-end
-
-return setmetatable(TextEditor, {
-	__call = function(_, ...) return TextEditor.new(...) end,
-})
+return TextEditor
